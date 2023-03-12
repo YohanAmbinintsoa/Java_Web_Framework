6676352463a5b4f50f6d344368d78af7a88c5e76
YohanAmbinintsoa <118614922+YohanAmbinintsoa@users.noreply.github.com>
2023-03-12 22:44:24 +0300

    Sprint2

---
+++ src/java/ETU1795/framework/Mapping.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ETU1795.framework;
+
+/**
+ *
+ * @author yohan
+ */
+public class Mapping {
+    String className;
+    String Method;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return Method;
+    }
+
+    public void setMethod(String Method) {
+        this.Method = Method;
+    }
+    
+    
+}
